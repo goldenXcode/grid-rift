0a0f842af57f2be097fb69e66eb8bc8e80628810
Senthose <Senthose@gmail.com>
2011-06-23 23:41:37 +0000

    Everything has changed

--- src/org/andeng/test/Ball.java
+++ src/org/andeng/test/Ball.java
@@ -1,19 +1,16 @@
 package org.andeng.test;
 
-import org.anddev.andengine.engine.handler.collision.ICollisionCallback;
-import org.anddev.andengine.entity.shape.IShape;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
 import com.badlogic.gdx.math.Vector2;
 
-public class Ball extends Sprite implements ICollisionCallback{
+public class Ball extends Sprite {
 	
 	private Vector2 velocity;
-	private String data;
-	private Vector2 angleVector;
 	private boolean alive;
 	private boolean onPlatform;
+	private Sprite platform;
 	private int lives;
 	
 	Ball(float x, float y, TextureRegion texture, int lives, Sprite platform){
@@ -22,12 +19,12 @@ public class Ball extends Sprite implements ICollisionCallback{
 		this.alive = true;
 		this.lives = lives;
 		this.onPlatform = true;
-		this.setParent(platform);
+		this.platform = platform;
 	}
 
 	public void move() {
 		if(onPlatform){
-			this.setPosition(this.getParent());
+			this.setPosition(platform.getX()+platform.getWidth()/2-10,platform.getY()-platform.getHeight()/2-2);
 		}
 		else if(alive){
 			this.setPosition(this.getX()+velocity.x, this.getY()+velocity.y);
@@ -44,7 +41,13 @@ public class Ball extends Sprite implements ICollisionCallback{
 	
 	public void launch(){
 		this.onPlatform = false;
-		this.setVelocity(new Vector2(-8, 8));
+		this.setVelocity(8, 8);
+	}
+	
+	public void die(){
+		this.lives--;
+		this.setVelocity(0, 0);
+		this.onPlatform = true;
 	}
 	
 	@Override
@@ -52,44 +55,13 @@ public class Ball extends Sprite implements ICollisionCallback{
 		this.move();
 		super.onManagedUpdate(pSecondsElapsed);
 	}
-
-	@Override
-	public boolean onCollision(IShape pCheckShape, IShape pTargetShape) {
-		//Figure out what the ball collided with
-		//If it was a wall negate the correct direction
-		data = (String) pTargetShape.getUserData();
-		if(data.compareTo("left") == 0 | data.compareTo("right") == 0){
-			velocity.x = -1 * velocity.x;
-			return false;
-		}
-		else if(data.compareTo("roof") == 0){
-			velocity.y = -1 * velocity.y;
-			return false;
-		}
-		else if(data.compareTo("ground") == 0){ //Hit the ground ball stops
-			//To-DO game over stuff
-			this.alive = false;
-			return false;
-		}
-		else if(data.compareTo("platform") == 0){//If it hits anything else figure out bounce based of off angles, still needs a little work in some special case...
-			float targetx = (pTargetShape.getX()-pTargetShape.getWidth()/2);
-			float targety = (pTargetShape.getY()-pTargetShape.getHeight()/2);
-			float ballx = (this.getX()-this.getWidth()/2);
-			float bally = (this.getY()-this.getHeight()/2);
-			this.angleVector = new Vector2((ballx-targetx),(bally-targety));
-			double angle = Math.atan(angleVector.y/angleVector.x);
-			if(angle > 0.245 | angle < -0.245){
-				velocity.x = -1 * velocity.x;
-			}
-			else{
-				velocity.y = -1 * velocity.y;
-			}
-			//Debug.d("Angle Vector: " + angleVector + " Angle: " + angle*(180/Math.PI));
-			//Debug.d("Upper Cutoff: " + 0.245*(180/Math.PI) + " Lower Cutoff" + -0.245*(180/Math.PI));
-			return true;
-		}
-		
-		return false;
+	
+		public void xShift(){
+		velocity.x = -1 * velocity.x;
+	}
+	
+	public void yShift(){
+		velocity.y = -1 * velocity.y;
 	}
 
 	public void setLives(int lives) {
@@ -100,12 +72,21 @@ public class Ball extends Sprite implements ICollisionCallback{
 		return lives;
 	}
 	
-	public void setVelocity(Vector2 velocity) {
-		this.velocity = velocity;
+	public void increaseLives(){
+		this.lives++;
+	}
+	
+	public void setVelocity(float x, float y) {
+		this.velocity.x = x;
+		this.velocity.y = y;
 	}
 
 	public Vector2 getVelocity() {
 		return velocity;
+	}
+	
+	public boolean isOnPlatform(){
+		return onPlatform;
 	}
 	
 }
